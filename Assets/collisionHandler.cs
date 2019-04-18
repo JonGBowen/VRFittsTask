25cf8377990724098c9e591f0b15d8e70cbe676f
Jon Bowen <jonbowen@live.ca>
2019-04-18 18:29:58 -0400

    add the stuff

---
+++ Assets/collisionHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionHandler : MonoBehaviour
+{
+
+    public static double increment_amplitude(double level)
+    {
+        return level * 0.04 + 0.08;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            transform.Translate(new Vector3(-0.04f,0,0));
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            transform.Translate(new Vector3(0.04f, 0, 0));
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
